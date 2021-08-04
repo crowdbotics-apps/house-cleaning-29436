09c8eeba05cdc033ad37e914e36f7b94759a9dff
Crowdbotics <team@crowdbotics.com>
2021-08-04 02:11:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "house_cleaning_29436Identifier",
+  url: "https://house_cleaning_29436Identifier.botics.co",
+  api: "https://house_cleaning_29436Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
